b01aa8cf7671dfe02a23dca07fef7aa91a01b425
Edyane Araujo <annearaujo034@gmail.com>
2022-06-16 21:52:30 -0300

    Adicionado arquivos

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //Programa básico q lê 3 números e mostra o maior entre eles ^u^
+        {
+            Console.WriteLine("Digite três números inteiros: ");
+
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            if (n1 > n2 && n1 > n3)
+            {
+                Console.WriteLine("Maior =" + n1);
+
+            }
+
+            else if (n2 > n3) 
+            {
+                Console.WriteLine("Maior =" + n2);
+            }
+            else
+            {
+                Console.WriteLine("Maior =" + n3);
+            }
+
+        }
+    }
+}
